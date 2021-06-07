37416ca380151e252057d01dd3c7de5021614ae1
matheusroquedebrito <79827371+matheusroquedebrito@users.noreply.github.com>
2021-06-07 13:47:54 -0300

    Create projeto01.java

---
+++ projeto01.java
@@ -0,0 +1,9 @@
+public class teste{
+    public static void main(String[] arg)
+    {
+      System.out.printf("%s%n%s%n",
+      "ola mundo",
+      "esse é o meu primeiro codigo em java");
+    }
+    
+    }
